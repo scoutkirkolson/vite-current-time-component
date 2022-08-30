7a6e4129c66114abff9811734c690b40d62f1d65
Louck Sitskoorn <louck@clockassist.com>
2022-08-30 15:42:29 +0200

    vite config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/vite-current-time-component/'
-    : '/',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vite-current-time-component/'
     : '/',
