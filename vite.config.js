5906a39498d6420361530b520b20af1db12ea51b
Louck Sitskoorn <louck@clockassist.com>
2022-09-01 13:03:30 +0200

    lib deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './main.js',
+      formats: ['es'],
+      fileName: 'vite-current-time',
+      name: 'ViteCurrentTime',
+    },
+    //rollupOptions: {
+    //  // Externalize deps that shouldn't be bundled into the library.
+    //  external: ['vue', '@vueuse/core'],
+    //},
+    sourcemap: true,
+    // Reduce bloat from legacy polyfills.
+    target: 'esnext',
+    // Leave minification up to applications.
+    minify: false,
+  },
   plugins: [vue({ customElement: true })],
   base: '/vite-current-time-component',
 });
